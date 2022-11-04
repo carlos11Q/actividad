6097832c2aef5c4aedc70504a229f90222560966
carlos11Q <carlospapel09@gmail.com>
2022-11-03 21:18:54 -0500

    cambiox

---
+++ ApicCarlos/Controllers/PersonajeController.cs
@@ -0,0 +1,34 @@
+﻿using ApicCarlos.Data;
+using DomainId.Modelo;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ApicCarlos.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonajeController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        public PersonajeController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Personaje>> CrearPersonaje([FromBody] PersonajeResponse i)
+        {
+            Personaje personaje = new Personaje();
+
+            personaje.Nombre = i.Nombre;
+            personaje.Poder = i.Poder;
+            personaje.Color = i.Color;
+            personaje.Fkgenero = i.Fkgenero;
+
+            personaje = _context.personajes
+
+            return personaje;
+        }
+
+    }
+}
--- DomainId/Modelo/Personaje.cs
+++ DomainId/Modelo/Personaje.cs
@@ -13,13 +13,23 @@ namespace DomainId.Modelo
         [Key]
         public int PkPersonaje { get; set; }
         public string Nombre { get; set; }
-        public string Color  { get; set; }
+        public string Color { get; set; }
         public string Poder { get; set; }
 
         [ForeignKey("genero")]
         public int Fkgenero { get; set; }
 
-       public Genero genero { get; set; }
+        public Genero genero { get; set; }
+
+    }
+
+    public class PersonajeResponse
+    {
+        public string Nombre { get; set; }
+        public string Color { get; set; }
+        public string Poder { get; set; }
+        public int Fkgenero { get; set; }
+
 
     }
 }
